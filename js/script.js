ded3c2215a8562d9e8a874f91476f0c951a70e67
Weronika <weronikaa1@gmail.com>
2020-12-25 18:27:04 +0100

    xd

---
+++ js/script.js
@@ -0,0 +1,59 @@
+const data = [
+    {
+        prodName: "Torebka Guess",
+        prodDescription: "Not Coordinated PW7380 PO381",
+        price: "279,0 zł",
+        promoPrice: null,
+        link: "https://www.eobuwie.com.pl/torebka-guess-not-coordinated-accessories-pw7380-p0381-bla.html",
+
+    },
+    {
+        prodName: "Szal Gino Rossi",
+        prodDescription: "O3W4-004-AW20",
+        price: "149,99 zł",
+        promoPrice: null,
+        link: "https://www.eobuwie.com.pl/szal-gino-rossi-o3w4-004-aw20-light-pink.html?snrai_campaign=cZSs64x8Tw9U&snrai_source=&snrai_content=&snrai_id=abd87455c0cf3e5caf51f8c2f090dbf7",
+    },
+    {
+        prodName: "Naszyjnik Fossil",
+        prodDescription: "Mosaic Heart JF03164791",
+        price: "239,0 zł",
+        promoPrice: null,
+        link: "https://www.eobuwie.com.pl/szal-gino-rossi-o3w4-004-aw20-light-pink.html?snrai_campaign=cZSs64x8Tw9U&snrai_source=&snrai_content=&snrai_id=abd87455c0cf3e5caf51f8c2f090dbf7",
+    },
+    {
+        prodName: "Duży Portfel Damski Love MOSCHINO",
+        prodDescription: "JC5633PP0BKA0000",
+        price: "439,0 zł",
+        promoPrice: null,
+        link: "https://www.eobuwie.com.pl/duzy-portfel-damski-love-moschino-jc5633pp0bka0000-nero.html",
+    },
+    {
+        prodName: "Zestaw kosmetyczek Guess",
+        prodDescription: "Did I Say 90? Accessories PWDDIS P0450",
+        price: '329,0 zł',
+        promoPrice: null,
+        link: "https://www.eobuwie.com.pl/zestaw-kosmetyczek-guess-did-i-say-90-accessories-pwddis-p0450-bml.html",
+    },
+    {
+        prodName: "Pasek Damski Gino Rossi",
+        prodDescription: "O3W2-005-AW20",
+        price: "99,99 zł",
+        promoPrice: null,
+        link: "https://www.eobuwie.com.pl/pasek-damski-gino-rossi-o3w2-005-aw20-black.html?frontendContext=JTdCJTIyc2VhcmNoRGF0YSUyMiUzQSU3QiUyMnNlYXJjaFVybCUyMiUzQSUyMi9zLmh0bWwlMjIlMkMlMjJzZWFyY2hRdWVyeSUyMiUzQSUyMk8zVzItMDA1LUFXMjAlMjIlMkMlMjJzZWFyY2hRdWVyeUNvbXBsZXhpdHklMjIlM0ExJTJDJTIyc2VhcmNoUGFnZVR5cGUlMjIlM0ElMjJzZWFyY2hyZXN1bHRzJTIyJTJDJTIyc2VhcmNoU3VnZ2VzdGlvbnMlMjIlM0ElN0IlMjJzZWFyY2hTdWdnZXN0aW9uc1VzZSUyMiUzQXRydWUlMkMlMjJzZWFyY2hTdWdnZXN0aW9uc1R5cGUlMjIlM0ElMjJwcm9kdWN0JTIyJTJDJTIyc2VhcmNoU3VnZ2VzdGlvbnNUYXJnZXQlMjIlM0ElMjIzNTMzNDkxJTIyJTdEJTdEJTdE",
+    },
+    {
+        prodName: "Zestaw upominkowy Wittchen",
+        prodDescription: "89-S-113",
+        price: "219 zł",
+        promoPrice: null,
+        link: "https://www.eobuwie.com.pl/szal-gino-rossi-o3w4-004-aw20-light-pink.html?snrai_campaign=cZSs64x8Tw9U&snrai_source=&snrai_content=&snrai_id=abd87455c0cf3e5caf51f8c2f090dbf7",
+    },
+    {
+        prodName: "Naszyjnik FOSSIL",
+        prodDescription: "Mosaic Heart JF03164791",
+        price: "149,99 zł",
+        promoPrice: null,
+        link: "https://www.eobuwie.com.pl/szal-gino-rossi-o3w4-004-aw20-light-pink.html?snrai_campaign=cZSs64x8Tw9U&snrai_source=&snrai_content=&snrai_id=abd87455c0cf3e5caf51f8c2f090dbf7",
+    },
+];
